63c13959e4d03afc4fcd5348c809084ea2c75138
Lucas Rocha <lucasrochadnsx@gmail.com>
2026-04-05 23:33:28 -0300

    atua

--- backend/src/main/java/br/com/dsmeta/dsmeta/resource/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/br/com/dsmeta/dsmeta/resource/exceptions/ResourceExceptionHandler.java
@@ -1,7 +1,5 @@
 package br.com.dsmeta.dsmeta.resource.exceptions;
 
-import br.com.dsmeta.dsmeta.service.exceptions.SmsException;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,15 +10,18 @@ import java.time.Instant;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-    @ExceptionHandler(SmsException.class)
-    public ResponseEntity<StandardError> entityNotFound(SmsException e, HttpServletRequest request) {
-        HttpStatus status = HttpStatus.NOT_FOUND;
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<StandardError> resourceNotFound(Exception e) {
+
         StandardError err = new StandardError();
         err.setTimestamp(Instant.now());
-        err.setStatus(status.value());
-        err.setError("Error de envio  SMS");
+        err.setStatus(404);
+        err.setError("Resource not found");
         err.setMessage(e.getMessage());
-        err.setPath(request.getRequestURI());
-        return ResponseEntity.status(status).body(err);
+        err.setPath("/movies");
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
 }
