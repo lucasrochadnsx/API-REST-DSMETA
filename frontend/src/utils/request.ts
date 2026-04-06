1e7235d7308e8a9c32c24234c65e77119b28485d
Lucas Rocha <lucasrochadnsx@gmail.com>
2026-04-05 22:43:31 -0300

    correções

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://api-rest-dsmeta.onrender.com";
